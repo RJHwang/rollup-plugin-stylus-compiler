1976596e738e145ff0497e84cf74352f339dc51e
dragon <rongjihuang@gmail.com>
2016-12-12 12:01:21 +0800

    add default support for .stylus extension

--- src/index.js
+++ src/index.js
@@ -1,36 +1,44 @@
 import { createFilter } from 'rollup-pluginutils'
-import stylus from 'stylus'
-
-const ext = /\.styl$/;
-const convertedExt = '.styl.css'
+import compiler from 'stylus'
 
 export default function(options = {}) {
-  const styles = {}
-  if (!options.include) options.include = '**/*.styl'
+  // set default stylus file extensions
+  if (!options.include) options.include = ['**/*.styl', '**/*.stylus']
+
   const filter = createFilter(options.include, options.exclude);
+
+  // use to cache the compiled content
+  // structure: {[compiledId]: [compiledContent], originId: [originId]}
+  const compiledCache = {}
 
   return {
     name: 'rollup-plugin-stylus-compiler',
     resolveId(importee, importer) {
-      if (importee.endsWith(convertedExt)) return importee
+      // if importee is a compiled id then return it.
+      // This will make it transmit to next plugin directly.
+      if (compiledCache[importee]) return importee
     },
     load(id) {
-      if (id.endsWith(convertedExt)) return styles[id] || ''
+      // if id is a compiled id then return the compiled content. 
+      // No need to transform again.
+      if (compiledCache[id]) return compiledCache[id]
     },
     transform(code, id) {
-      if (!ext.test(id)) return null
       if (!filter(id)) return null
       return new Promise(function(resolve, reject) {
-        stylus(code).render(function(err, css) {
+        compiler(code).render(function(err, css) {
           if (err) reject(err)
           else {
-            // cache the converted content
-            const cssFile = id + '.css'
-            styles[cssFile] = css
+            // cache the compiled content
+            // use .css extention so next plugin can deal it as pure css
+            const compiledId = id + '.css'
+
+            compiledCache[compiledId] = css
+            compiledCache['originId'] = id
 
             resolve({
               // make next css plugin work
-              code: `import ${JSON.stringify(cssFile)}`,
+              code: `import ${JSON.stringify(compiledId)}`,
               map: { mappings: '' }
             })
           }
