a321dd22a76cc62ee53a1889eea4ac56c4fa7005
RJ <rongjihuang@gmail.com>
2019-03-12 22:51:56 +0800

    Simplify build script by new output option
    See :
    1. [RFC: Standardise options and CLI flags #1479](https://github.com/rollup/rollup/issues/1479)
    2. [Unifying Rollup options](https://gist.github.com/Rich-Harris/d472c50732dab03efeb37472b08a3f32)

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,10 @@ var external = Object.keys(require('./package.json').dependencies);
 
 export default {
   input: 'src/index.js',
+  output: [
+    { file: 'dist/rollup-plugin-stylus-compiler.cjs.js', format: 'cjs'},
+    { file: 'dist/rollup-plugin-stylus-compiler.es.js', format: 'es'}
+  ],
   external: external.concat(['path']),
   plugins: [buble()]
 };
