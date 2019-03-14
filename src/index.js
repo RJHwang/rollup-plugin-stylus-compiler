d8bbb93544aee5a68667b49b1030ef9efa79440a
RJ <rongjihuang@gmail.com>
2019-03-14 23:52:54 +0800

    Polishing src

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { createFilter } from 'rollup-pluginutils'
-import compiler from 'stylus'
+import stylus from 'stylus'
 import path from 'path'
 
-const name = 'rollup-plugin-stylus-compiler'
-const debug = require('debug')(name)
-
-export default function(options = {}) {
+export default function (options = {}) {
   // set default stylus file extensions
   if (!options.include) options.include = ['**/*.styl', '**/*.stylus']
 
@@ -15,55 +12,39 @@ export default function(options = {}) {
   // structure: {[compiledId]: [compiledContent]}
   const compiledCache = {}
 
+  /** API : https://rollupjs.org/guide/en#hooks */
   return {
-    name: name,
-    /**
-     * Rollup default to defer to the next plugin `resolveId` function (return 
-     * null or undefined). Because of the compiled id (importee) is created by 
-     * this plugin and is not originally exists. So for compiled id (importee), 
-     * for avoid transmit it to the next plugin to resolve it again, here need 
-     * to return the compiled id itself. That means all next plugins `resolveId` 
-     * function will not call any more for the compiled id.
-     */
+    name: 'rollup-plugin-stylus-compiler',
+    /** Kind: async, first */
     resolveId(importee, importer) {
-      debug('resolveId importee=%s, importer=%s', importee, importer)
       if (compiledCache[importee]) return importee
     },
-    /**
-     * Rollup default to load content from file system (return null or undefined).
-     * Because of the compiled id is created by this plugin and is not really 
-     * exists in the file system. So for compiled id, here need to return the c
-     * ompiled content directly (The compiled content is created and cached by 
-     * the `transform` function). That means all next plugins `load` function 
-     * will not call any more for the compiled id.
-     */
+    /** Kind: async, first */
     load(id) {
-      debug('load id=%s', id)
       if (compiledCache[id]) return compiledCache[id]
     },
+    /** Kind: async, sequential */
     transform(code, id) {
-      debug('transform id=%s, code=%s', id, code)
       if (!filter(id)) return
-      return new Promise(function(resolve, reject) {
-        const stylus = compiler(code, options.compiler);
-        const relativePath = path.relative(process.cwd(), id);
-        stylus.set('filename', relativePath);
-        stylus.render(function(err, css) {
-          if (err) reject(err)
-          else {
-            // cache the compiled content
-            // use `.css` extention so next plugin can deal it as pure css
-            const compiledId = id + '.css'
+      return new Promise(function (resolve, reject) {
+        const relativePath = path.relative(process.cwd(), id)
+        stylus(code, options.compiler)
+          .set('filename', relativePath)
+          .render(function (err, css) {
+            if (err) reject(err)
+            else {
+              // cache the compiled content
+              // use `.css` extention so next plugin can deal it as pure css
+              const compiledId = `${id}.css`
+              compiledCache[compiledId] = css
 
-            compiledCache[compiledId] = css
-
-            resolve({
-              // make next css plugin work
-              code: `import ${JSON.stringify(compiledId)}`,
-              map: { mappings: '' }
-            })
-          }
-        })
+              resolve({
+                // make next css plugin work
+                code: `import ${JSON.stringify(compiledId)}`,
+                map: { mappings: '' }
+              })
+            }
+          })
       })
     }
   }
